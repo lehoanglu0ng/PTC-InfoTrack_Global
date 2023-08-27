dc8874c5fecfa8c0ffa4c425208480bd42d2c9ab
lehoanglu0ng <lehoanglu0ng.vn@gmail.com>
2023-08-27 13:04:36 +0700

    e2e test case

--- steps_file.js
+++ steps_file.js
@@ -15,5 +15,34 @@ module.exports = function () {
       this.amBearerAuthenticated(secret(response.data.token));
     },
 
+    register: function (user) {
+      this.sendPostRequest('/register', user)
+      this.seeResponseCodeIsSuccessful()
+    },
+
+    login: function (user) {
+      this.sendPostRequest('/login', user)
+      this.seeResponseCodeIsSuccessful()
+      this.seeResponseValidByCallback(({ data, status, expect }) => {
+        this.amBearerAuthenticated(secret(data.token))
+      })
+    },
+
+    createUser: async function (user) {
+      const response = await this.sendPostRequest('/users', user)
+      this.seeResponseCodeIsSuccessful()
+      return response.data.id
+    },
+
+    updateUser: function (id, data) {
+      this.sendPutRequest(`/users/${id}`, data)
+      this.seeResponseCodeIsSuccessful()
+    },
+
+    deleteUser: function (id) {
+      this.sendDeleteRequest(`/users/${id}`)
+      this.seeResponseCodeIsSuccessful()
+    }
+
   });
 }
---
+++ tests/E2E.js
@@ -0,0 +1,25 @@
+const { faker } = require("@faker-js/faker");
+const Joi = require('joi');
+
+Feature('End to End');
+
+Scenario.only('Update and delete new user by new admin account workflow', async ({ I }) => {
+    const admin = {
+        "email": "eve.holt@reqres.in",
+        "password": "pistol"
+    }
+    const user = {
+        name: faker.person.fullName(),
+        job: faker.person.jobTitle()
+    }
+    const newData = {
+        name: faker.person.fullName(),
+        job: faker.person.jobTitle()
+    }
+    
+    I.register(admin)
+    I.login(admin)
+    const userID = await I.createUser(user)
+    I.updateUser(userID, newData)
+    I.deleteUser(userID)
+});
